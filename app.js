127c2ad469b8390e891bcfb5cdb887d5458ae839
ahp336 <aakashparikh8@gmail>
2019-07-09 23:23:16 -0400

    final Addition of HTML and JS files with some very minor styling

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 
 
-var animals = ["Monkey", "Eagle" , "Lion"];
+var animals = ["Monkey","Eagle","Lion"];
 
 function animalsDisplayInfo () {
     var animal = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=gTinIy9p5s6ihXYdVmmHTHobf3BACXga&q=" + animal 
-    + "&limit=25&offset=0&rating=G&lang=en";
+    + "&limit=10&offset=0&rating=R&lang=en";
 
     $.ajax({
         url : queryURL,
         method : "GET"
-    }).then(function(response){
-        var results = response.data;
-        console.log(results);
-        for (var i=0 ; i < result.length; i++){
-            var animalDiv = $("<div class = 'animal'>");
+    }).then(function(salty){
+        var results = salty.data;
+        
+        for(var i=0; i< results.length; i++){
+            console.log(results[i].url);
+            var animalDiv = $("<div class='animal'>");
             var rating = results[i].rating
             var p = $("<p>").text("Rating: " + rating);
             var animalImage = $("<img>")
-
-            animalImage.attr("src", results[i].images.fixed_width.url);
+            
+            animalImage.attr("src", results[i].images.fixed_height.url);
 
             animalDiv.append(p);
             animalDiv.append(animalImage);
 
-            $("#aniaml-gifs").prepend(animalDiv);
-            
+            $("#animal-gifs").prepend(animalDiv);
         }
     });
 }
@@ -33,11 +33,11 @@ function animalsDisplayInfo () {
 function animalButtons(){
     $("#buttons-view").empty();
 
-    for(var i=0; i< animals.lengths; i++){
+    for(var i=0; i< animals.length; i++){
         var XZ = $("<button>");
         XZ.addClass("animal-btn");
         XZ.attr("data-name", animals[i]);
-        XZ.text(aniamls[i]);
+        XZ.text(animals[i]);
 
         $("#buttons-view").append(XZ);
 
@@ -45,8 +45,13 @@ function animalButtons(){
 }
 
 $("#add-animals").on("click" , function(){
-    var animal = $("animal-input").val().trim();
-    animals.push(animal);
+    event.preventDefault();
+
+    var animal1 = $("#animal-input").val();
+    animals.push(animal1);
     animalButtons();
 
 });
+
+$(document).on("click", ".animal-btn", animalsDisplayInfo);
+animalButtons();
